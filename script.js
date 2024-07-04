84394bbcca7ef07bac6189db10871d074d7e12f7
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2024-07-05 01:26:50 +0530

    done

--- script.js
+++ script.js
@@ -4,4 +4,15 @@
     $("#home-page,#store,#mac-section").hide();
     $($(this).attr("href")).show();
   });
+
+
+  // --------------------- M A C - S E C T I O N -------------------------
+
+  // -------------- P R O D U C T ----------------
+ $('#desktop-sec,#display-sec').hide()
+
+ $(".product-button").on("click", function () {
+    $("#laptops-sec,#desktop-sec,#display-sec").hide();
+    $($(this).attr("href")).show();
+  });
   
