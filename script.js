a21ac6844d9f775025a0fcc8131c0346d14d86e5
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2024-07-20 20:50:42 +0530

    done

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
 // --------------------- N A V I G A T I O N ------------------------
 
-$("#home-page,#store,#ipad-section,#mac-section,#iphone-section,#watch-section,#airpods-section,#tv-and-home-section").hide();
+$("#home-page,#store,#ipad-section,#mac-section,#iphone-section,#watch-section,#airpods-section,#tv-and-home-section,#entertainment-section").hide();
 
 $(".nav-link").on("click", function () {
-  $("#home-page,#store,#mac-section,#ipad-section,#iphone-section,#watch-section,#airpods-section,#tv-and-home-section,#entertainment-section").hide();
+  $("#home-page,#store,#mac-section,#ipad-section,#iphone-section,#watch-section,#airpods-section,#tv-and-home-section,#entertainment-section,#accessories-section").hide();
   $($(this).attr("href")).show();
 });
 
